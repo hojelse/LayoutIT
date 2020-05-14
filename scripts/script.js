70168dda1ee354a33fc57dd2224b54314fbd92fe
emiljapelt <ejapelt@hotmail.com>
2020-05-14 14:56:40 +0200

    add text

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,19 @@ window.onload = function() {
     document.getElementById("left").onclick = goLeft;
     document.getElementById("right").onclick = goRight;
     document.getElementById("addPage").onclick = addPage;
+    document.getElementById("addText").onclick = addTextField;
 
     document.onkeydown = firekey;
 
     window.onresize = resizePage;
 
-    addPage();
+    pageAmount++;
+    collPages.push(new Page(pageAmount));
+    
+    currPage++;
     changeToCurrPage();
 
     resizePage();
-    
 }
 
 function firekey(e) {
@@ -37,11 +40,13 @@ var collPages = [];
 class Page {
     constructor(pageNum) {
         this.pageNum = pageNum;
+        this.texts = [];
     }
 }
 
 function goLeft() {
     if(currPage != 1){
+        savePage();
         currPage -= 1;
         changeToCurrPage();
     }
@@ -49,6 +54,7 @@ function goLeft() {
 
 function goRight() {
     if(currPage != pageAmount){
+        savePage();
         currPage += 1;
         changeToCurrPage();
     }
@@ -57,7 +63,30 @@ function goRight() {
 function changeToCurrPage() {
     var page = document.getElementById('page');
 
+    page.innerHTML = "";
     page.innerHTML = collPages[currPage-1].pageNum;
+
+    var textlist = collPages[currPage-1].texts;
+    
+    for(var i = 0; i < textlist.length; i++){
+        var textField = document.createElement('input');
+        textField.classList.add("pagetext");
+        textField.setAttribute("type", "text");
+        textField.style.border = 'none';
+        textField.placeholder = "Enter text here";
+        textField.size = 16;
+        textField.addEventListener('keyup', function() {
+            var target = event.target;
+            var text = target.value;
+            if(text.length < 16) {
+                target.size = 16;
+            } else {
+                target.size = text.length + 6;
+            }
+        })
+        textField.value = textlist[i];
+        page.appendChild(textField);
+    }
 
     if(currPage == pageAmount){
         document.getElementById('addPage').hidden = false;
@@ -72,8 +101,28 @@ function addPage() {
     pageAmount++;
     collPages.push(new Page(pageAmount));
     
+    savePage();
     currPage++;
     changeToCurrPage();
+}
+
+function addTextField() {
+    var textField = document.createElement('input');
+    textField.classList.add("pagetext");
+    textField.setAttribute("type", "text");
+    textField.style.border = 'none';
+    textField.placeholder = "Enter text here";
+    textField.size = 16;
+    textField.addEventListener('keyup', function() {
+        var target = event.target;
+        var text = target.value;
+        if(text.length < 16) {
+            target.size = 16;
+        } else {
+            target.size = text.length + 6;
+        }
+    })
+    document.getElementById('page').appendChild(textField);
 }
 
 // var pctPageW = 80;
@@ -96,5 +145,16 @@ function resizePage() {
         pageContainer.style.flexDirection = "row";
         page.style.height = pageContainer.offsetWidth * aHeight + "px";
         page.style.width = "auto";
+    }
+}
+
+function savePage() {
+    var page = collPages[currPage-1];
+    var textlist = page.texts;
+    textlist.splice(0,textlist.length);
+
+    var pagetexts = document.getElementsByClassName('pagetext');
+    for(var i = 0; i < pagetexts.length; i++){
+        textlist.push(pagetexts[i].value);
     }
 }
